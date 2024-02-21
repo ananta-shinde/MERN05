37fe67af33cda50c00be2a2de383a616ccd0c05a
aShinde <ananta.shinde@honeywellcom>
2024-02-21 14:49:15 +0530

    21/02/2024

--- web-components/product-listing/main.js
+++ web-components/product-listing/main.js
@@ -5,15 +5,70 @@ var products =[ {
     name : "Motorola G34 5G (Ocean Green, 128 GB)  (8 GB RAM)",
     price: 25000,
     rating: 4.2,
-    discount : 50
+    discount : 50,
+    reviews : [
+        {
+            user_name:"Ananta Shinde",
+            comment : "Fabulous Product"
+        },
+        {
+            user_name:"virat Kohli",
+            comment : "Bad Product"
+        },
+        {
+            user_name:"Anushka Sharma",
+            comment : "Good Product"
+        },
+        {
+            user_name:"Anushka Sharma",
+            comment : "Good Product"
+        }
+    ]
 },{
     id : 2,
     image_url:"https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/g/j/d/-original-imagtqqddnkayg4x.jpeg?q=70",
     name : "Motorola G34 5G (Ocean Green, 128 GB)  (8 GB RAM)",
     price: 25000,
     rating: 4.2,
-    discount : 20
+    discount : 20,
+    reviews : [
+        {
+            user_name:"Ananta Shinde",
+            comment : "Fabulous Product"
+        },
+        {
+            user_name:"virat Kohli",
+            comment : "Bad Product"
+        },
+        {
+            user_name:"Anushka Sharma",
+            comment : "Good Product"
+        },
+        {
+            user_name:"Robin Sharma",
+            comment : "Good Product"
+        }
+    ]
 }
+]
+
+var categories = [
+    {
+        id: 1,
+        name:"Electronics"
+    },
+    {
+        id: 2,
+        name:"Mobiles"
+    },
+    {
+        id: 3,
+        name:"Cloths"
+    },
+    {
+        id: 4,
+        name:"Groceries"
+    }
 ]
 
 var root = document.getElementById("root");
@@ -37,5 +92,23 @@ var structure = "";
 
 root.innerHTML = structure;
  
+
+
+
+var reviews = products[0].reviews
+var productDetails = document.getElementById("product-details")
+// for(i=0;i<reviews.length;i++)
+// {
+//    productDetails.innerHTML = productDetails.innerHTML+ `<div class="card">
+//     <h4>${reviews[i].user_name}</h4>
+//     <p> ${reviews[i].comment}</p>
+//     </div>`
+// }
+
+productDetails.innerHTML = reviews.map((r)=>(`<div class="card">
+<h4>${r.user_name}</h4>
+<p> ${r.comment}</p>
+</div>`))
+
 
 
