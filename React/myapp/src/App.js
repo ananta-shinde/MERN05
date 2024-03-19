e57bcde0d99fefea18df1f0c1719d16cd3364ad0
aShinde <ananta.shinde@honeywellcom>
2024-03-19 15:13:38 +0530

    19/03/24

--- React/myapp/src/App.js
+++ React/myapp/src/App.js
@@ -25,10 +25,45 @@ function Counter(props)
     </>
    )
 }
+
+function TodoList()
+{
+  const [toDoList,setTodoList] = useState([])
+  // const toDoList = [
+  //   {taskId:1,description :"Learn HTML"},
+  //   {taskId:2,description :"Learn CSS"},
+  //   {taskId:3,description :"Learn React"}
+  // ];
+  
+  
+  const handleSave = () =>{
+        //  toDoList.push({taskId:4,description:"Learn JS"})
+         setTodoList([...toDoList,{taskId:4,description:"Learn JS"}])
+  }
+  return(
+    <>
+      <div className='todo-container'>
+          <h4>Welcome to to do list app</h4>
+          <input placeholder='Enter task to do' type='text'/>
+          <button onClick={handleSave}>Save</button>
+          <ul className='task-list'>
+            {
+               toDoList.map((task)=>(
+                <li className='task-item' id={task.taskId}><input type='checkbox'/>{task.description}</li>
+               ))
+            }
+          </ul>
+          <p>No task to do</p>
+       </div>
+    </>
+  )
+}
+
 function App() { 
   return (
     <>
-       <Counter btnIncText="increment" btnDecText="decrment"/>
+       <Counter/>
+       <TodoList/>
     </>
     
   );
