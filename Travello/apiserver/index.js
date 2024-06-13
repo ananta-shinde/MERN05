357cac6c1e2564174e5b1dc21f86eb9ee7f24c0c
aShinde <ananta.shinde@honeywellcom>
2024-06-13 15:01:20 +0530

    13/06/24

--- Travello/apiserver/index.js
+++ Travello/apiserver/index.js
@@ -1,24 +1,22 @@
 // import express module
 const express = require("express")
-
+const appData = require("./data.json")
 // create an app object
 const app = express()
- 
 
-const rootRequestHandler = (req,res) =>{
-    console.log("request was done")
-    res.send("your request was listend")
-}
+app.get("/",(req,res)=>{
+      
+})
 
+app.get("/products",(req,res)=>{
+    console.log(req.query)
+    console.log(req.params)
+    res.send(appData.products)
+})
 
-// handling request
-app.get("/",rootRequestHandler)
-
-app.get("/products",(req,res)=>{ 
-    res.send([{
-        id:1,
-        name:"p1"
-    }])
+app.get("/products/:id",(req,res)=>{
+    
+    res.send(appData.products.find(p=>p.id==req.params.id))
 })
 
 
